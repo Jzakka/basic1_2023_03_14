7517c3118185d133fa70bb610e98a6e941c9c4fd
Jzakka <mouse4786@gmail.com>
2023-03-14 13:25:22 +0900

    feat: 13강

---
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -0,0 +1,67 @@
+package com.ll.basic1;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    private static int count = 0;
+    private static int id = 1;
+
+    private static ArrayList<Person> persons = new ArrayList<>();
+
+    @GetMapping("/main2")
+    @ResponseBody
+    public String  hello() {
+        return "반가웠습니다.";
+    }
+
+    @GetMapping("/main3")
+    @ResponseBody
+    public String  hello2() {
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int  increase() {
+        return count++;
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int plus(
+            @RequestParam(defaultValue = "0") int a,
+            int b) {
+        return a+b;
+    }
+
+    @GetMapping("/addPerson")
+    @ResponseBody
+    public String addPerson(String name, int age){
+        persons.add(new Person(id, name, age));
+        return String.format("%d번 사람이 추가되었습니다.", id++);
+    }
+
+    @GetMapping("/people")
+    @ResponseBody
+    public Person[] getPeople() throws JsonProcessingException {
+        return persons.toArray(Person[]::new);
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    class Person{
+        int id;
+        String name;
+        int age;
+    }
+}
