4668e09cfc19c570a6514cf5d8cecd95da54669a
Jzakka <mouse4786@gmail.com>
2023-03-17 11:08:21 +0900

    feat: 47강

--- src/main/java/com/ll/basic1/boundedcontext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedcontext/article/entity/Article.java
@@ -1,12 +1,22 @@
 package com.ll.basic1.boundedcontext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.*;
 
 @Entity
 public class Article {
-    @Id
+
+    @Id //PK
+    @GeneratedValue(strategy = IDENTITY) //AUTO_INCREMENT
     private long id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title;
     private String body;
 }
